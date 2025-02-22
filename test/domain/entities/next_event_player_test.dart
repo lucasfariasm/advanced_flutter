bf987410a71e297077ba1af65a85547d786c8850
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-02-22 16:17:02 -0300

    return correct initials in an empty name

--- test/domain/entities/next_event_player_test.dart
+++ test/domain/entities/next_event_player_test.dart
@@ -39,7 +39,7 @@ class NextEventPlayer {
 
   static String _getInitials(String name) {
     final names = name.toUpperCase().split(' ');
-    final firstChar = names.first[0];
+    final firstChar = names.first.split('').firstOrNull ?? '-';
     final lastChar =
         names.last.split('').elementAtOrNull(names.length == 1 ? 1 : 0) ?? '';
     return '$firstChar$lastChar';
@@ -61,6 +61,10 @@ void main() {
   test('should return the first letters of the first name', () {
     expect(initialsOf('Lucas'), 'LU');
     expect(initialsOf('L'), 'L');
+  });
+
+  test('should return "-" when name is empty', () {
+    expect(initialsOf(''), '-');
   });
 
   test('should convert to uppercase', () {
