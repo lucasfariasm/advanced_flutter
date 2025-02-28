419eb6e3f628b9ea8f5bd8ff0b4c9aea56072793
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-02-28 10:57:42 -0300

    create typealias to improve readability

--- test/infra/api/repositories/load_next_event_api_repo_test.dart
+++ test/infra/api/repositories/load_next_event_api_repo_test.dart
@@ -5,6 +5,9 @@ import 'package:flutter_test/flutter_test.dart';
 
 import '../../../helpers/fakes.dart';
 
+typedef Json = Map<String, dynamic>;
+typedef JsonArr = List<Json>;
+
 class LoadNextEventApiRepository implements LoadNextEventRepository {
   final HttpGetClient httpClient;
   final String url;
@@ -13,15 +16,15 @@ class LoadNextEventApiRepository implements LoadNextEventRepository {
 
   @override
   Future<NextEvent> loadNextEvent({required String groupId}) async {
-    final json = await httpClient
-        .get<Map<String, dynamic>>(url: url, params: {"groupId": groupId});
+    final json =
+        await httpClient.get<Json>(url: url, params: {"groupId": groupId});
 
     return NextEventMapper.toObject(json);
   }
 }
 
 class NextEventMapper {
-  static NextEvent toObject(Map<String, dynamic> json) => NextEvent(
+  static NextEvent toObject(Json json) => NextEvent(
         groupName: json['groupName'],
         date: DateTime.parse(json['date']),
         players: NextEventPlayerMapper.toList(json['players']),
@@ -29,10 +32,10 @@ class NextEventMapper {
 }
 
 class NextEventPlayerMapper {
-  static List<NextEventPlayer> toList(List<Map<String, dynamic>> arr) =>
+  static List<NextEventPlayer> toList(JsonArr arr) =>
       arr.map(NextEventPlayerMapper.toObject).toList();
 
-  static NextEventPlayer toObject(Map<String, dynamic> json) => NextEventPlayer(
+  static NextEventPlayer toObject(Json json) => NextEventPlayer(
         id: json['id'],
         name: json['name'],
         position: json['position'],
