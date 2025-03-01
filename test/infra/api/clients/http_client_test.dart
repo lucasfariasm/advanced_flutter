094ea0195778dfa01a9b4779e8f8e69137f70cc3
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-01 17:45:16 -0300

    ensure HttpClient throws UnexpectedError on 400

--- test/infra/api/clients/http_client_test.dart
+++ test/infra/api/clients/http_client_test.dart
@@ -1,3 +1,5 @@
+import 'package:advanced_flutter/domain/entities/domain_error.dart';
+
 import 'package:dartx/dartx.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
@@ -21,7 +23,13 @@ class HttpClient {
         {'content-type': 'application/json', 'accept': 'application/json'},
       );
     final uri = _buildUri(url: url, params: params, queryString: queryString);
-    await client.get(uri, headers: allHeaders);
+    final response = await client.get(uri, headers: allHeaders);
+    switch (response.statusCode) {
+      case 200:
+        break;
+      default:
+        throw DomainError.unexpected;
+    }
   }
 
   Uri _buildUri({
@@ -112,6 +120,12 @@ void main() {
         params: {'p3': 'v3', 'p4': 'v4'},
       );
       expect(client.url, 'http://anyurl.com/v3/v4?q1=v1&q2=v2');
+    });
+
+    test('should throw UnexpectedError on 400', () async {
+      client.simulateBadRequestError();
+      final future = sut.get(url: url);
+      expect(future, throwsA(DomainError.unexpected));
     });
   });
 }
