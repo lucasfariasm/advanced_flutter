bd44339bee75bab5ccea2cfc801919f6d8f46a2e
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-03 17:36:27 -0300

    apply class modifiers

--- lib/domain/usecases/next_event_loader.dart
+++ lib/domain/usecases/next_event_loader.dart
@@ -1,12 +1,12 @@
 import 'package:advanced_flutter/domain/entities/next_event.dart';
 import 'package:advanced_flutter/domain/repositories/load_next_event_repo.dart';
 
-class NextEventLoader {
+final class NextEventLoader {
 // propertie injection
 //   LoadNextEventRepository? repo;
   final LoadNextEventRepository repo;
 
-  NextEventLoader({required this.repo});
+  const NextEventLoader({required this.repo});
 
   Future<NextEvent> call({required String groupId}) async {
     return repo.loadNextEvent(groupId: groupId);
