5631b1044040cd5fd1bcdfe110318953057e7356
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-05 16:05:12 -0300

    move NextEventPresenter to its own file

--- test/ui/pages/next_event_player_test.dart
+++ test/ui/pages/next_event_player_test.dart
@@ -1,30 +1,10 @@
+import 'package:advanced_flutter/presentation/presenters/next_event_presenter.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:rxdart/subjects.dart';
 
 import '../../helpers/fakes.dart';
-
-final class NextEventViewModel {
-  final List<NextEventPlayerViewModel> goalkeepers;
-  final List<NextEventPlayerViewModel> players;
-  final List<NextEventPlayerViewModel> out;
-  final List<NextEventPlayerViewModel> doubt;
-
-  const NextEventViewModel({
-    this.goalkeepers = const [],
-    this.players = const [],
-    this.out = const [],
-    this.doubt = const [],
-  });
-}
-
-final class NextEventPlayerViewModel {
-  final String name;
-
-  const NextEventPlayerViewModel({
-    required this.name,
-  });
-}
 
 final class NextEventPage extends StatefulWidget {
   final NextEventPresenter presenter;
@@ -110,11 +90,6 @@ final class ListSection extends StatelessWidget {
       ],
     );
   }
-}
-
-abstract class NextEventPresenter {
-  Stream<NextEventViewModel> get nextEventStram;
-  void loadNextEvent({required String groupId});
 }
 
 final class NextEventPresenterSpy implements NextEventPresenter {
