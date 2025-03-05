a612f75d3b63d536645cac4fa7803dd25cc1405b
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-05 16:53:36 -0300

    should present red color when confirmation is false

--- test/ui/components/player_status_test.dart
+++ test/ui/components/player_status_test.dart
@@ -9,14 +9,16 @@ class PlayerStatus extends StatelessWidget {
     required this.isConfirmed,
   });
 
+  Color getColor() => isConfirmed ? Colors.teal : Colors.pink;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       width: 16,
       height: 16,
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         shape: BoxShape.circle,
-        color: Colors.teal,
+        color: getColor(),
       ),
     );
   }
@@ -33,5 +35,17 @@ void main() {
         .firstWidget<Container>(find.byType(Container))
         .decoration as BoxDecoration;
     expect(decoration.color, Colors.teal);
+  });
+
+  testWidgets('should present red status', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: PlayerStatus(isConfirmed: false),
+      ),
+    );
+    final decoration = tester
+        .firstWidget<Container>(find.byType(Container))
+        .decoration as BoxDecoration;
+    expect(decoration.color, Colors.pink);
   });
 }
