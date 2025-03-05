0eada32e1c7dda1396b01429cf97e2cdf5c25aca
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-05 16:56:43 -0300

    should present grey color when confirmation is null

--- test/ui/components/player_status_test.dart
+++ test/ui/components/player_status_test.dart
@@ -2,14 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 class PlayerStatus extends StatelessWidget {
-  final bool isConfirmed;
+  final bool? isConfirmed;
 
   const PlayerStatus({
     super.key,
-    required this.isConfirmed,
+    this.isConfirmed,
   });
 
-  Color getColor() => isConfirmed ? Colors.teal : Colors.pink;
+  Color getColor() => switch (isConfirmed) {
+        true => Colors.teal,
+        false => Colors.pink,
+        null => Colors.blueGrey,
+      };
 
   @override
   Widget build(BuildContext context) {
@@ -47,5 +51,17 @@ void main() {
         .firstWidget<Container>(find.byType(Container))
         .decoration as BoxDecoration;
     expect(decoration.color, Colors.pink);
+  });
+
+  testWidgets('should present grey status', (tester) async {
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: PlayerStatus(isConfirmed: null),
+      ),
+    );
+    final decoration = tester
+        .firstWidget<Container>(find.byType(Container))
+        .decoration as BoxDecoration;
+    expect(decoration.color, Colors.blueGrey);
   });
 }
