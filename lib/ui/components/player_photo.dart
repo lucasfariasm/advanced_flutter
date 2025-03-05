271d244937b8ea5df8869e2bf1d8cf325f1d8240
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-05 17:12:28 -0300

    move PlayerPhoto to its own file

---
+++ lib/ui/components/player_photo.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class PlayerPhoto extends StatelessWidget {
+  final String initials;
+  final String? photo;
+
+  const PlayerPhoto({
+    super.key,
+    required this.initials,
+    this.photo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      foregroundImage: photo != null ? NetworkImage(photo!) : null,
+      child: photo == null ? Text(initials) : null,
+    );
+  }
+}
