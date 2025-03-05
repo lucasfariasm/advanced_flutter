754466f46a983b74fd268a96485bef5e3323ed66
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-05 15:49:56 -0300

    ensure NextEventPage hide doubt section

--- test/ui/pages/next_event_player_test.dart
+++ test/ui/pages/next_event_player_test.dart
@@ -264,5 +264,6 @@ void main() {
     expect(find.text("DENTRO - GOLEIROS"), findsNothing);
     expect(find.text("DENTRO - JOGADORES"), findsNothing);
     expect(find.text("FORA"), findsNothing);
+    expect(find.text("DÚVIDA"), findsNothing);
   });
 }
