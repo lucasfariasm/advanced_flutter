3d22e65c9db147f46d9758080efee22f3ae87b40
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-05 11:39:28 -0300

    ensure NextEventPage hide spinner on load error

--- test/ui/pages/next_event_player_test.dart
+++ test/ui/pages/next_event_player_test.dart
@@ -58,6 +58,10 @@ final class NextEventPresenterSpy implements NextEventPresenter {
     nextEventSubject.add('');
   }
 
+  void emitError() {
+    nextEventSubject.addError(Error());
+  }
+
   @override
   void loadNextEvent({required String groupId}) {
     loadCallsCount++;
@@ -96,6 +100,14 @@ void main() {
     await tester.pumpWidget(sut);
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
     presenter.emitNextEvent();
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+
+  testWidgets('should hide spinner on load error', (tester) async {
+    await tester.pumpWidget(sut);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    presenter.emitError();
     await tester.pump();
     expect(find.byType(CircularProgressIndicator), findsNothing);
   });
