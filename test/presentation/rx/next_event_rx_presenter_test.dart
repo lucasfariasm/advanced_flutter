19581eacdcbf4e3255f86a919dbe2c35b588720d
Lucas Farias de Moura <lucasfarias.bsb@hotmail.com>
2025-03-06 10:37:48 -0300

    ensure NextEventRxPresenter emits correct events on reload with success

--- test/presentation/rx/next_event_rx_presenter_test.dart
+++ test/presentation/rx/next_event_rx_presenter_test.dart
@@ -27,6 +27,7 @@ final class NextEventRxPresenter {
       await nextEventLoader(groupId: groupId);
     } catch (error) {
       nextEventSubject.addError(error);
+    } finally {
       if (isReload) isBusySubject.add(false);
     }
   }
@@ -73,5 +74,10 @@ void main() {
     expectLater(sut.nextEventStream, emitsError(nextEventLoader.error));
     sut.isBusyStream.listen(neverCalled);
     await sut.loadNextEvent(groupId: groupId);
+  });
+
+  test('should emit correct events on reload with success', () async {
+    expectLater(sut.isBusyStream, emitsInOrder([true, false]));
+    await sut.loadNextEvent(groupId: groupId, isReload: true);
   });
 }
